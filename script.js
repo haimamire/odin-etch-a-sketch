30b138fe71f31a9703531075937c1beab7cf4693
haimamire <82563520+haimamire@users.noreply.github.com>
2025-06-05 14:45:36 +0900

    Setup grid using just flexbox

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
 const contentDiv = document.querySelector(".content");
 
-const gridSize = 16;
-for (let i = 0; i < gridSize; i++) {
-    const row = document.createElement("div");
-    row.className = "row";
-    contentDiv.appendChild(row);
-
-    for (let i = 0; i < gridSize; i++) {
+function createGrid() {
+    const gridSize = 16;
+    
+    for (let i = 0; i < (gridSize * gridSize); i++) {
         const square = document.createElement("div");
         square.className = "square";
-        row.appendChild(square);
+        contentDiv.appendChild(square);
     }
 }
+
+createGrid();
