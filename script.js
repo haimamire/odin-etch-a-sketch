9d09928cad86550cbaf79a93ffa7fd3a5546c3fd
haimamire <82563520+haimamire@users.noreply.github.com>
2025-06-05 15:13:23 +0900

    Make every div a perfect square

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
 const contentDiv = document.querySelector(".content");
 
 function createGrid() {
-    const gridSize = 16;
+    const gridSize = 8;
     
     for (let i = 0; i < (gridSize * gridSize); i++) {
         const square = document.createElement("div");
         square.className = "square";
+        // This flex-basis is here to define the number of divs in a row, based on the grid size.
+        // And the padding-top is to make each div a square.
+        square.style.flexBasis = `${100 / gridSize}%`;
+        square.style.paddingTop = `${100 / gridSize}%`;
+
         contentDiv.appendChild(square);
     }
 }
